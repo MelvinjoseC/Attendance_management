ddcb8f960124fa979b52dd8aae09ba4a9317bce8
MELVIN <melvinjos025@gmail.com>
2023-05-10 00:09:32 +0530

    Attendance management

--- Attendance_Management/Course_app/admin.py
+++ Attendance_Management/Course_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from.models import Qualifications_name, Statename
+from.models import Qualifications_name, Statename, District, Batches
 # Register your models here.
 
 admin.site.register(Qualifications_name)
 admin.site.register(Statename)
+admin.site.register(District)
+admin.site.register(Batches)
 
